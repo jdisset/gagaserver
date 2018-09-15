0bb7b246cb4e69c9ce1505fcec054f5216ccddfb
Jean Disset <jdisset@mit.edu>
2018-09-15 04:12:20 -0400

    comments

--- client/client.py
+++ client/client.py
@@ -3,9 +3,11 @@ import time
 
 
 
-# example of eval function
+# example of eval function; receives a list of dnas (already parsed y json)
 def maxSum(dnaList):
     return [{'fitnesses':{'sum':sum(i['values'])}} for i in dnaList]
 
+# worker creation
 worker = GAGAWorker("tcp://localhost:4321", maxSum, batchSize = 20)
+
 worker.start()
