79b3486defffcb34f4cf683a17dc750ca223204f
Jean Disset <jdisset@mit.edu>
2018-12-11 18:34:33 -0500

    fitness evaluation in client simple exemple

--- client/client.py
+++ client/client.py
@@ -7,8 +7,7 @@ import time
 # example of eval function; receives a list of dnas (already parsed y json)
 def maxSum(dnaList):
     parsedList = [json.loads(i) for i in dnaList]
-    # return [{'fitnesses':{'sum':sum(i['values'])}} for i in parsedList]
-    r= [{'footprint':[[1,2,3],[0,1,2]]} for i in parsedList]
+    r= [{'fitnesses':{'sum':sum(i['values'])}} for i in parsedList]
     print(json.dumps(r))
     return r
 
